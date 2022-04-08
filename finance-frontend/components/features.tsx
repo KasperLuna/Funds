9e6d931832a25c7464794d13dc63cebcfc2cbe96
KasperLuna <dev@kasperluna.com>
2022-04-08 17:19:49 +0800

    Updated feature section

--- finance-frontend/components/features.tsx
+++ finance-frontend/components/features.tsx
@@ -1,138 +1,110 @@
 import React from "react";
 import {
-  ThemeIcon,
-  Text,
-  Title,
-  Container,
-  SimpleGrid,
-  useMantineTheme,
   createStyles,
+  Title,
+  SimpleGrid,
+  Text,
+  Button,
+  ThemeIcon,
+  Grid,
+  Col,
 } from "@mantine/core";
-import { Icon as TablerIcon } from "tabler-icons-react";
-
-interface FeatureProps {
-  icon: TablerIcon;
-  title: React.ReactNode;
-  description: React.ReactNode;
-}
-
-import { Gauge, Cookie, User, Message2, Lock } from "tabler-icons-react";
-
-export const MOCKDATA = [
-  {
-    icon: Gauge,
-    title: "Extreme performance",
-    description:
-      "This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit",
-  },
-  {
-    icon: User,
-    title: "Privacy focused",
-    description:
-      "People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma",
-  },
-  {
-    icon: Cookie,
-    title: "No third parties",
-    description:
-      "They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves",
-  },
-  {
-    icon: Lock,
-    title: "Secure by default",
-    description:
-      "Although it still can’t fly, its jumping power is outstanding, in Alola the mushrooms on Paras don’t grow up quite right",
-  },
-  {
-    icon: Message2,
-    title: "24/7 Support",
-    description:
-      "Rapidash usually can be seen casually cantering in the fields and plains, Skitty is known to chase around after its own tail",
-  },
-];
-
-export function Feature({ icon: Icon, title, description }: FeatureProps) {
-  const theme = useMantineTheme();
-  return (
-    <div>
-      <ThemeIcon variant="light" size={40} radius={40}>
-        <Icon style={{ width: 20, height: 20 }} />
-      </ThemeIcon>
-      <Text style={{ marginTop: theme.spacing.sm, marginBottom: 7 }}>
-        {title}
-      </Text>
-      <Text size="sm" color="dimmed" style={{ lineHeight: 1.6 }}>
-        {description}
-      </Text>
-    </div>
-  );
-}
+import {
+  ReceiptOff,
+  Flame,
+  CircleDotted,
+  FileCode,
+  ChartAreaLine,
+  Eye,
+} from "tabler-icons-react";
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
-    paddingTop: theme.spacing.xl * 4,
-    paddingBottom: theme.spacing.xl * 4,
+    padding: `${theme.spacing.xl * 2}px ${theme.spacing.xl}px`,
   },
 
   title: {
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 36,
     fontWeight: 900,
+    lineHeight: 1.1,
     marginBottom: theme.spacing.md,
-    textAlign: "center",
-
-    [theme.fn.smallerThan("sm")]: {
-      fontSize: 28,
-      textAlign: "left",
-    },
-  },
-
-  description: {
-    textAlign: "center",
-
-    [theme.fn.smallerThan("sm")]: {
-      textAlign: "left",
-    },
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
   },
 }));
 
-interface FeaturesGridProps {
-  title: React.ReactNode;
-  description: React.ReactNode;
-  data?: FeatureProps[];
-}
+const features = [
+  {
+    icon: ReceiptOff,
+    title: "Free. Forever.",
+    description:
+      "Financhee will always be free, leaving users' money where it can stay and grow.",
+  },
+  {
+    icon: FileCode,
+    title: "Open-Source",
+    description:
+      "We know you're concerned with where your data goes, so we've made Financhee open-source to let users decide.",
+  },
+  {
+    icon: ChartAreaLine,
+    title: "Smart Charts",
+    description:
+      "Show only what you need and not a pixel more, That's what we believe in and stand by with giving users the control over their visualizations.",
+  },
+  {
+    icon: Eye,
+    title: "Flexible",
+    description:
+      "Giving you control over how the tools look and feel is how we respect you and your eyes, no more blinding webpages at night.",
+  },
+];
 
-export function FeaturesGrid({
-  title,
-  description,
-  data = MOCKDATA,
-}: FeaturesGridProps) {
+export function FeaturesTitle() {
   const { classes } = useStyles();
-  const theme = useMantineTheme();
-  const features = data.map((feature, index) => (
-    <Feature {...feature} key={index} />
+
+  const items = features.map((feature) => (
+    <div key={feature.title}>
+      <ThemeIcon
+        size={44}
+        radius="md"
+        variant="gradient"
+        gradient={{ deg: 133, from: "blue", to: "cyan" }}
+      >
+        <feature.icon size={26} />
+      </ThemeIcon>
+      <Text size="lg" mt="sm" weight={500}>
+        {feature.title}
+      </Text>
+      <Text color="dimmed" size="sm">
+        {feature.description}
+      </Text>
+    </div>
   ));
 
   return (
-    <Container className={classes.wrapper}>
-      <Title className={classes.title}>{title}</Title>
-
-      <Container size={560} p={0}>
-        <Text size="sm" className={classes.description}>
-          {description}
-        </Text>
-      </Container>
-
-      <SimpleGrid
-        mt={60}
-        cols={3}
-        spacing={theme.spacing.xl * 2}
-        breakpoints={[
-          { maxWidth: 980, cols: 2, spacing: "xl" },
-          { maxWidth: 755, cols: 1, spacing: "xl" },
-        ]}
-      >
-        {features}
-      </SimpleGrid>
-    </Container>
+    <div className={classes.wrapper}>
+      <Grid gutter={80} px={50} pt={50}>
+        <Col span={12} md={5}>
+          <Title className={classes.title} order={2}>
+            The streamlined approach to managing and controlling your financial
+            assets.
+          </Title>
+          <Text color="dimmed">
+            Track and manage your financial assets with ease – Financhee gives
+            users the edge in monitoring where their finances go and grow.
+          </Text>
+        </Col>
+        <Col span={12} md={7}>
+          <SimpleGrid
+            cols={2}
+            spacing={30}
+            breakpoints={[{ maxWidth: "md", cols: 1 }]}
+          >
+            {items}
+          </SimpleGrid>
+        </Col>
+      </Grid>
+    </div>
   );
 }
