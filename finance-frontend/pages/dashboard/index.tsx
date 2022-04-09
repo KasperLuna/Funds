ed9e5c4f6c03aa54851c4feb5b1d9f21a9d53ead
KasperLuna <dev@kasperluna.com>
2022-04-09 23:55:30 +0800

    updated navbar styling

--- finance-frontend/pages/dashboard/index.tsx
+++ finance-frontend/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 //import styles from "../styles/Home.module.css";
-import { NavbarMinimalColored } from "../../components/dashboard/navBar";
+import { NavbarMinimalColored } from "../../components/dashboard/NavBar";
 
 const Home: NextPage = () => {
   return (
