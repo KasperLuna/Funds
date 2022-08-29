54d598d3ea00d290d46c8c0857c297d937965d1a
KasperLuna <dev@kasperluna.com>
2022-08-29 11:50:53 +0800

    add pwa items

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -11,6 +11,16 @@ export default function App(props: AppProps) {
       <Head>
         <title>Page title</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, user-scalable=no"
+        />
       </Head>
 
       <MantineProvider
