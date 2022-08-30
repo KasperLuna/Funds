d304ebf97e253a8592c89471404a3638ae627a12
Kasper Luna <dev@kasperluna.com>
2022-08-30 12:45:44 +0800

    added nextjs progressbar

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavHeader from "./NavHeader";
+import NextNProgress from "nextjs-progressbar";
 import { Container } from "@mantine/core";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <NextNProgress
+        color="rgba(255, 255, 255, 0.4)"
+        startPosition={0.3}
+        stopDelayMs={0}
+        height={2}
+      />
       <NavHeader />
       <Container
         size={"xl"}
