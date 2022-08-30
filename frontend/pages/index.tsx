b08d67ce30196311f9ff923c2f06e698c94ecb8f
Kasper Luna <dev@kasperluna.com>
2022-08-30 12:22:20 +0800

    remove unused imports

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { ActionIcon, Group } from "@mantine/core";
-import Create from "../components/Create";
-import TransactionList from "../components/TransactionList";
-import { IconFilter, IconSettings } from "@tabler/icons";
-import { BankStats } from "../components/BankStats";
 
 const Home: NextPage = () => {
   return (
