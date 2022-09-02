d9eb6f6b90037fe8f937c13cc9bb5c1c9502560a
KasperLuna <dev@kasperluna.com>
2022-09-02 08:44:05 +0800

    remove old files

--- finance-backend/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.json({ response: "Success" });
-});
-
-export default router;
