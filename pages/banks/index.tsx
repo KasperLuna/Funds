0ac32319a2f0acb56e2adad087e930d1af50ca0f
KasperLuna <dev@kasperluna.com>
2023-04-04 00:07:52 +0800

    Separated transaction layout toggle into button

--- pages/banks/index.tsx
+++ pages/banks/index.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { ActionIcon, Anchor, Breadcrumbs, Group, Tooltip } from "@mantine/core";
+import { Anchor, Breadcrumbs, Group } from "@mantine/core";
 import Create from "../../components/banks/Create";
 import TransactionList from "../../components/banks/TransactionList";
 import { BankStats } from "../../components/banks/BankStats";
 import Link from "next/link";
 import { Filter } from "../../components/banks/Filter";
-import { useTxLayout } from "../../utils/helpers";
-import { IconLayoutGrid, IconTable } from "@tabler/icons";
+import { TransactionLayoutButton } from "../../components/banks/TransactionLayoutButton";
 
 const Home: NextPage = () => {
-  const { txLayout, setTxLayout } = useTxLayout();
-
-  const isTableLayout = txLayout === "table";
-
-  const toggleLayoutQuery = () => {
-    txLayout === "table" ? setTxLayout("card") : setTxLayout("table");
-  };
-
   const pages = [{ title: "Banks", href: "/banks" }].map((page) => (
     <Link href={page.href} key={page.title} passHref>
       <Anchor>{page.title}</Anchor>
@@ -35,19 +26,7 @@ const Home: NextPage = () => {
       <BankStats />
       <Group position="apart">
         <Group>
-          <Tooltip
-            label={
-              isTableLayout ? "Toggle to Card View" : "Toggle to Table View"
-            }
-          >
-            <ActionIcon onClick={() => toggleLayoutQuery()}>
-              {isTableLayout ? (
-                <IconLayoutGrid size={24} stroke={1.5} />
-              ) : (
-                <IconTable size={24} stroke={1.5} />
-              )}
-            </ActionIcon>
-          </Tooltip>
+          <TransactionLayoutButton />
           <Filter />
         </Group>
         <Create />
