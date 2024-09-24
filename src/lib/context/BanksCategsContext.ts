11748f36c288000eec2e18d8cac4125bc9a415f5
KasperLuna <dev@kasperluna.com>
2024-09-24 19:58:14 +0800

    fix: adjust bank adding and cache clearing

--- src/lib/context/BanksCategsContext.ts
+++ src/lib/context/BanksCategsContext.ts
@@ -2,8 +2,16 @@ import { createContext } from "react";
 import { Bank, Category, Currency } from "../types";
 
 export type BanksCategsContextType = {
-  bankData?: { banks: Bank[]; loading: boolean };
-  categoryData?: { categories: Category[]; loading: boolean };
+  bankData?: {
+    banks: Bank[];
+    loading: boolean;
+    refetch: () => Promise<unknown>;
+  };
+  categoryData?: {
+    categories: Category[];
+    loading: boolean;
+    refetch: () => Promise<unknown>;
+  };
   baseCurrency?: Currency;
 };
 
