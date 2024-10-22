fd1a2500eb0adf5fbca026779da01d90c08f91c9
KasperLuna <dev@kasperluna.com>
2024-10-23 00:20:55 +0800

    fix: disable next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import withPWA from "next-pwa";
+// import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,33 +9,35 @@ const nextConfig = {
   },
 };
 
-export default withPWA({
-  dest: "public", // destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
-  register: true, // register the PWA service worker
-  skipWaiting: true, // skip waiting for service worker activation
-  runtimeCaching: [
-    {
-      // Cache everything except requests to pb.kasperluna.com
-      urlPattern: ({ url }) => {
-        return !url.hostname.includes("pb.kasperluna.com");
-      },
-      handler: "CacheFirst",
-      options: {
-        cacheName: "default-cache",
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      // Bypass cache for pb.kasperluna.com
-      urlPattern: ({ url }) => url.hostname.includes("pb.kasperluna.com"),
-      handler: "NetworkOnly",
-    },
-  ],
-})(nextConfig);
+// export default withPWA({
+//   dest: "public", // destination directory for the PWA files
+//   disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+//   register: true, // register the PWA service worker
+//   skipWaiting: true, // skip waiting for service worker activation
+//   runtimeCaching: [
+//     {
+//       // Cache everything except requests to pb.kasperluna.com
+//       urlPattern: ({ url }) => {
+//         return !url.hostname.includes("pb.kasperluna.com");
+//       },
+//       handler: "CacheFirst",
+//       options: {
+//         cacheName: "default-cache",
+//         expiration: {
+//           maxEntries: 200,
+//           maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+//         },
+//         cacheableResponse: {
+//           statuses: [0, 200],
+//         },
+//       },
+//     },
+//     {
+//       // Bypass cache for pb.kasperluna.com
+//       urlPattern: ({ url }) => url.hostname.includes("pb.kasperluna.com"),
+//       handler: "NetworkOnly",
+//     },
+//   ],
+// })(nextConfig);
+
+export default nextConfig;
