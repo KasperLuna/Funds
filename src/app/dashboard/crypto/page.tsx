9573350dabbdb4a5aae9bccd0b4b83d0fd2b4973
KasperLuna <dev@kasperluna.com>
2025-04-01 15:09:49 +0800

    fix: crypto export

--- src/app/dashboard/crypto/page.tsx
+++ src/app/dashboard/crypto/page.tsx
@@ -1,7 +1,6 @@
 import { Bitcoin } from "lucide-react";
-import dynamic from "next/dynamic";
 
-function Page() {
+export default function Page() {
   return (
     <main className="flex flex-col h-[calc(100dvh-245px)] items-center p-24 text-slate-200">
       <Bitcoin className="w-28 h-28" />
@@ -9,7 +8,3 @@ function Page() {
     </main>
   );
 }
-
-export default dynamic(() => Promise.resolve(Page), {
-  ssr: false,
-});
