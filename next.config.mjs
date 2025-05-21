47621761a4f08662d883793b0b1b343c2ae1352c
KasperLuna <dev@kasperluna.com>
2025-05-22 01:28:40 +0800

    fix: add coingecko to image pattern

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+        port: "",
+        pathname: "/coins/images/**",
+      },
+    ],
+  },
 };
 
 // export default withPWA({
