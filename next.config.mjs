3907517899c3860e065734cd50c44ffe1db1e1c2
KasperLuna <dev@kasperluna.com>
2025-05-29 01:12:57 +0800

    fix: revert day-picker

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
