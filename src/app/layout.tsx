40dc74d81289f013ec8783fa33d1b318687edb77
KasperLuna <dev@kasperluna.com>
2026-04-20 23:31:09 +0800

    fix: scaling on PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const viewport: Viewport = {
   width: "device-width",
   height: "device-height",
   initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
   colorScheme: "dark",
 };
